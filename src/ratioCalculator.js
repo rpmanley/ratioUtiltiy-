710ac0af30e83cf23013e9219311e31b9ed5694e
Richard Manley <ricmanle@publicisgroupe.net>
2020-07-24 13:07:52 +0200

    introduce material ui

--- src/ratioCalculator.js
+++ src/ratioCalculator.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 class RatioCalc extends Component {
     constructor ( props ) {
@@ -28,7 +30,6 @@ class RatioCalc extends Component {
             w = inputs.a / gcd,
             h = inputs.b / gcd,
             ratio = w + ":" + h;
-            // console.log('-- gcd -- ' , gcd, ' ratio ', ratio, ' event.target ', event.target.getAttribute('id') );
 
           this.setState({ ratio: ratio });
 
@@ -37,12 +38,10 @@ class RatioCalc extends Component {
           if ( targetInput == 'c' ) {
               let d = ( inputs.b / inputs.a ) * inputs.c;
               inputs.d = d.toFixed(1);
-              console.log('changing d ', d, ' gcd ', gcd );
           }
           else if ( targetInput == 'd' )  {
               let c = ( inputs.a / inputs.b ) * inputs.d;
               inputs.c = c.toFixed(1);
-              console.log('changing c ');
           }
           else {
               inputs.c = inputs.d = '';
@@ -54,30 +53,41 @@ class RatioCalc extends Component {
     }
     render () {
         return(
-          <div className="ratioCalc">
-              <h2>React ratio calculator</h2>
-              <aside><a href="https://github.com/rpmanley/ratioUtiltiy-">Github</a></aside>
-              <table>
-                  <thead>
-                      <tr>
-                          <td><label htmlFor="a">A</label></td>
-                          <td><label htmlFor="b">B</label></td>
-                          <td><label htmlFor="c">C</label></td>
-                          <td><label htmlFor="d">D</label></td>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <td><input id="a" value={this.state.inputs.a} onChange={(e) => this.updateInput('a', e)} type="number" /></td>
-                          <td><input id="b" value={this.state.inputs.b} onChange={(e) => this.updateInput('b', e)} type="number" /></td>
-                          <td><input id="c" value={this.state.inputs.c} onChange={(e) => this.updateInput('c', e)} type="number" /></td>
-                          <td><input id="d" value={this.state.inputs.d} onChange={(e) => this.updateInput('d', e)} type="number" /></td>
-                      </tr>
-                  </tbody>
-              </table>
-              <label htmlFor="ratio">Ratio</label>
-              <input readOnly={true} id="ratio" value={this.state.ratio} />
-          </div>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            >
+              <div className="ratioCalc">
+                  <h2>React ratio calculator</h2>
+                  <aside><a href="https://github.com/rpmanley/ratioUtiltiy-">Github</a></aside>
+                  <table>
+                      <thead>
+                          <tr>
+                              <td><label htmlFor="a">A</label></td>
+                              <td><label htmlFor="b">B</label></td>
+                              <td><label htmlFor="c">C</label></td>
+                              <td><label htmlFor="d">D</label></td>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td><input id="a" value={this.state.inputs.a} onChange={(e) => this.updateInput('a', e)} type="number" /></td>
+                              <td><input id="b" value={this.state.inputs.b} onChange={(e) => this.updateInput('b', e)} type="number" /></td>
+                              <td><input id="c" value={this.state.inputs.c} onChange={(e) => this.updateInput('c', e)} type="number" /></td>
+                              <td><input id="d" value={this.state.inputs.d} onChange={(e) => this.updateInput('d', e)} type="number" /></td>
+                          </tr>
+                      </tbody>
+                  </table>
+                  <label htmlFor="ratio">Ratio</label>
+                  <input readOnly={true} id="ratio" value={this.state.ratio} />
+                  <Button variant="contained" color="primary">Reset</Button>
+                  <p>
+                      Using React, Material design - <a href="https://github.com/rpmanley/ratioUtiltiy-">Github</a>
+                  </p>
+              </div>
+          </Grid>
         )
     }
 }
